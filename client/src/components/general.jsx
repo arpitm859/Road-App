faf61ff743b1ad66dea2d7f58ec1c0226f68e37c
khushboogupta13 <guptakhushi345@gmail.com>
2021-01-16 16:33:46 +0530

    upvote

--- client/src/components/general.jsx
+++ client/src/components/general.jsx
@@ -3,46 +3,56 @@ import './general.css';
 import { Navbar, Nav} from 'react-bootstrap';
 import { Carousel } from 'react-responsive-carousel';
 
-const General = () => {
+const LandingPage = () => {
 
     return(
         <div>
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>Bombay Municipal Corporation</Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="/complains">Register a Complaint</Nav.Link>
-                <Nav.Link href="/existing-complains">Existing Complaints</Nav.Link>
-            </Nav>
-        </Navbar>
+        <Navbar style={{backgroundColor:"black"}}>
+                    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <Navbar.Brand class="nav-link" style={{color:"white"}}>Bombay Municipal Corporation</Navbar.Brand>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="mx-auto">
+                        
+                        <Nav>
+                            <Nav.Link href="/complains" style={{color:"white",paddingLeft:"1rem"}} class="navbar-brand mx-auto" >Register a Complaint</Nav.Link>
+                            
+                            <Nav.Link href="/existing-complaints" style={{color:"white",paddingLeft:"2rem"}} class="navbar-brand mx-auto" >Existing Complaints</Nav.Link>
+                            
+                            <Nav.Link href="/my-complaints" style={{color:"white",paddingLeft:"2rem"}} class="navbar-brand mx-auto" >My Complaints</Nav.Link>
+                        </Nav>
+                        
+                        
+                    </div>
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <Nav.Link href="/login" style={{color:"white"}} class="nav-link" >Logout</Nav.Link>
+                    </li>
+                </ul>
+    </div>
+            </Navbar>
         <Carousel>
             <Carousel.Item interval={1000}>
-                <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-                />
+                <img alt="First slide" />
                 <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={500}>
-                <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Third slide"
-                />
+                <img alt="Third slide"/>
                 <Carousel.Caption>
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-                />
+                <img alt="Third slide" />
                 <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
@@ -55,4 +65,4 @@ const General = () => {
 
 }
 
-export default General;
+export default LandingPage;
