382568f80bc34ec18275fe273384562668a956b5
Arp!T <arpitchatting@gmail.com>
2021-02-02 13:39:31 +0530

    made ui changes

--- client/src/pages/status/status.jsx
+++ client/src/pages/status/status.jsx
@@ -38,7 +38,7 @@ const Status = (props) => {
 						description={data.description}
 					/>
 					<iframe
-						src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1779.560539360676!2d80.98310177197504!3d26.867893997020815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjbCsDUyJzA0LjQiTiA4MMKwNTknMDIuNSJF!5e0!3m2!1sen!2sin!4v1612196761979!5m2!1sen!2sin'
+						src='https://www.google.com/maps/embed/v1/place?key=AIzaSyDdKd_hcdItqfkNDHWd7IQQD-UjML8yXEI&q=riverfront,gomti+nagar,lucknow,uttar+pradesh,india'
 						width='900'
 						height='450'
 						frameborder='0'
